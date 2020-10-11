8c4f0bcca170bd3e0b0ed0831d4b80c30c51758e
Niharkajla <niharkajla28@gmail.com>
2020-10-11 22:10:20 +0530

    first trial code

---
+++ sms_script.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = 'AC3ff4eb2cb93e5ddb3d07b64e03c1cb54'
+auth_token = '1b7b829979e9f1ad9d482be3f90d7f6e'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_='+12056914156',
+    body='I am learning how to send the messages through python',
+    to='+918050106439'
+)
+
+print(message.sid)
